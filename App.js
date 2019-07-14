f06e04e14a0ee801604e7944241337612cc87fbd
Jay <psatdoctor@naver.com>
2019-07-14 10:22:58 +0900

    Loading View

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+
+  render(){
+    const {isLoaded} = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Data Loaded</Text>
+          </View>
+          
+        )}
+        {/* <Text style={{fontSize: 100, backgroundColor: 'red'}}>shut the Apple</Text> */}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +29,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  loading: {
+    flex: 1,
+    backgroundColor: 'blue',
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  }, 
+  loadingText: {
+    fontSize: 38, 
+    marginBottom: 2
+  }
 });
